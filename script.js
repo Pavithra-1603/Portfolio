e3d4f8e14e390a8171f288fc472b607302b88ae8
Pavithra <pavithra1603@gmail.com>
2026-06-22 12:59:22 +0530

    Portfolio updated

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Portfolio Loaded");
+const text = "MCA Graduate | Python Developer";
+let i = 0;
+
+function typeWriter(){
+    if(i < text.length){
+        document.getElementById("typing").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeWriter,100);
+    }
+}
+
+typeWriter();
